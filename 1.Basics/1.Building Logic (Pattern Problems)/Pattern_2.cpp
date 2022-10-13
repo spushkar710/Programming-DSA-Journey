ce41dcd200943d44b789bc9da5267491d25c6ad1
Pushkar Singh <spushkar710@gmail.com>
2022-10-13 21:25:03 +0530

    made some changes

--- 1.Basics/1.Building Logic (Pattern Problems)/Pattern_2.cpp
+++ 1.Basics/1.Building Logic (Pattern Problems)/Pattern_2.cpp
@@ -17,7 +17,7 @@ using namespace std;
 class Solution {
   public:
     void printTriangle(int n) {
-        // code here
+        
         for(int row=0;row<n;row++)
         {
             for(int col=0;col<=row;col++)
